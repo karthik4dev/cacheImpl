369c67fc31851263e0b6e5f0e448593748e44f19
karthik4dev <karthikpnrao.97@gmail.com>
2024-12-26 21:36:01 +0530

    first commit

---
+++ src/main/java/com/karthikproject/cacheimpl/CacheimplApplication.java
@@ -0,0 +1,13 @@
+package com.karthikproject.cacheimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheimplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheimplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthikproject/cacheimpl/CacheimplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthikproject.cacheimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheimplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
