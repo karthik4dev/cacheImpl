e81f8b04ca0463488952ffbeff58955239b38357
karthik4dev <karthikpnrao.97@gmail.com>
2025-01-12 01:31:55 +0530

    Post Mapping added

--- src/main/java/com/karthikproject/cacheimpl/CacheimplApplication.java
+++ src/main/java/com/karthikproject/cacheimpl/CacheimplApplication.java
@@ -1,13 +1,20 @@
 package com.karthikproject.cacheimpl;
 
+import com.karthikproject.cacheimpl.Models.Employees;
+import com.karthikproject.cacheimpl.Services.EmployeeService;
+import com.karthikproject.cacheimpl.Services.EmployeeServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import java.util.ArrayList;
+
 @EnableCaching
+@Slf4j
 @SpringBootApplication
 public class CacheimplApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CacheimplApplication.class, args);
 	}
