8f0b70fccf7056e2703defdebc89e91265a6e8aa
karthik4dev <karthikpnrao.97@gmail.com>
2025-01-12 22:04:15 +0530

    Connection to oracle localhost DB added. Now it is working fine for Get and Post along with Cache.

--- src/main/java/com/karthikproject/cacheimpl/CacheimplApplication.java
+++ src/main/java/com/karthikproject/cacheimpl/CacheimplApplication.java
@@ -1,15 +1,9 @@
 package com.karthikproject.cacheimpl;
 
-import com.karthikproject.cacheimpl.Models.Employees;
-import com.karthikproject.cacheimpl.Services.EmployeeService;
-import com.karthikproject.cacheimpl.Services.EmployeeServiceImpl;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
-import java.util.ArrayList;
 
 @EnableCaching
 @Slf4j
