0d9b878a082d1908f0ca8bfd414aa8455b3d193b
karthik4dev <karthikpnrao.97@gmail.com>
2025-05-25 02:31:32 +0530

    Adding small tests for the configurations

--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 # Github repo to learn Cache utilization of the Employees DB. 
 
 1. it is a public repo
-2. usage is limited to Oracle DB. Can be configured to Mysql OR H2 DB.
+2. usage is limited to Oracle DB. Can be configured to Mysql OR H2 DB in application CConfigurations.
 3. ~~dont fork my repo as my updates will be gone :angry:~~ will be reviewed later if you push new update.
 4. Docker image coming soon ... :smirk:
+
+Normal Tests are being written over Mockito 
+Once i have time to learn Mockito , will be implemented for all classes.
+
--- src/test/java/com/karthikproject/cacheimpl/CacheimplApplicationTests.java
+++ src/test/java/com/karthikproject/cacheimpl/CacheimplApplicationTests.java
@@ -1,10 +1,47 @@
 package com.karthikproject.cacheimpl;
 
+import com.karthikproject.cacheimpl.Controllers.EmployeeController;
+import com.karthikproject.cacheimpl.Models.Employees;
+import com.karthikproject.cacheimpl.Services.EmployeeService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.util.Assert;
+
+import static org.springframework.util.Assert.isNull;
 
 @SpringBootTest
 class CacheimplApplicationTests {
+
+	@Autowired
+	EmployeeController employeeController;
+
+	@Autowired
+	EmployeeService employeeService;
+
+
+	@Test
+	void getEmployeeTest(){
+		Employees emp1= employeeController.getEmployeesByID(1).orElseThrow();
+
+		Assert.notNull(emp1,"Null");
+	}
+
+	@Test
+	@Transactional
+	@Rollback
+	void saveEmployeeTest(){
+		Employees emp1= Employees.builder().employeeId(1).employeeName("Karthik P N").deptID(2).email("kpnzeus@gmail.com").build();
+
+		employeeController.saveEmployee(emp1);
+		Employees emp2= employeeController.getEmployeesByID(1).orElseThrow();
+
+		Assert.notNull(emp2,"No Object Found");
+
+	}
 
 	@Test
 	void contextLoads() {
